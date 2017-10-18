b145b7bbee8de8fcd5cc43b843f5fb9208725ecc
guwashimaru <32773254+guwashimaru@users.noreply.github.com>
2017-10-18 15:29:11 +0900

    Uniqueness for user id
    Should not be taken the same user id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :user_id, presence: true
+  validates :user_id, presence: true, uniqueness: {case_sensitive: false}
   validates :password, presence: true
 end
