8e1bf247e1fdd3b00537f292aba535c9421e83e2
guwashimaru <32773254+guwashimaru@users.noreply.github.com>
2017-10-18 16:19:44 +0900

    Set has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :user_id, presence: true, uniqueness: {case_sensitive: true}
-  validates :password, presence: true
+  validates :password, presence: true, length: {minimun:6}
+  has_secure_password
+
 end
