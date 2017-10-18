5b91fa25acfa245a339930003447e5a555419718
guwashimaru <32773254+guwashimaru@users.noreply.github.com>
2017-10-18 15:30:25 +0900

    fix user id validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :user_id, presence: true, uniqueness: {case_sensitive: false}
+  validates :user_id, presence: true, uniqueness: {case_sensitive: true}
   validates :password, presence: true
 end
