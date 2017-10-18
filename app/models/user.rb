10cb6615317e72c6933a404d086b83e770b0f862
guwashimaru <32773254+guwashimaru@users.noreply.github.com>
2017-10-18 18:05:11 +0900

    Fix spell miss

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :user_id, presence: true, uniqueness: {case_sensitive: true}
-  validates :password, presence: true, length: {minimun:6}
+  validates :password, presence: true, length: {minimum:6}
   has_secure_password
 
 end
