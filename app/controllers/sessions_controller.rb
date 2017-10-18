54839b4b39a76977fec8ae9bb4a1f799a6b764d5
guwashimaru <32773254+guwashimaru@users.noreply.github.com>
2017-10-18 18:23:52 +0900

    Fix login view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
+    def create
+        user = User.find_by(user_id: params[:session][:user_id].downcase)
+        if user && user.authenticate(params[:session][:password])
+          reset_session
+          session[:user_id] = user.id
+          redirect_to user
+        else
+          flash.now[:danger] = 'Invalid user ID/password combination'
+          render 'new'
+        end
+      end
   end
 end
